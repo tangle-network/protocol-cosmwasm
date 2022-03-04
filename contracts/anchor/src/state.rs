b99534e0287f0a5d0b7e7f8fb536cd68cde9bf8f
guorong <dudurong009@gmail.com>
2022-03-04 06:46:46 -0700

    Implement the state of "anchor" contract
    Need to do research on how to use "MerkleTree" in cosmwasm, as in ink.

--- contracts/anchor/src/state.rs
+++ contracts/anchor/src/state.rs
@@ -1,10 +1,13 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{Addr, Storage, StdResult};
+use cosmwasm_std::{ Storage, StdResult, Uint256};
 use cw_storage_plus::{Item, Map};
 
 use protocol_cosmwasm::error::ContractError;
+use protocol_cosmwasm::poseidon::Poseidon;
+use protocol_cosmwasm::anchor_verifier::AnchorVerifier;
+use protocol_cosmwasm::zeroes;
 
 const ROOT_HISTORY_SIZE: u32 = 100;
 
@@ -24,7 +27,7 @@ pub struct Edge {
 pub struct LinkableMerkleTree {
     pub max_edges: u32,
     pub chain_id_list: Vec<ChainId>,
-    // pub edges: Mapping<ChainId, Edge>,
+    // pub edges: Map<ChainId, Edge>,
     // pub curr_neighbor_root_index: Mapping<ChainId, u32>,
     // pub neighbor_roots: Mapping<(ChainId, u32), [u8; 32]>,
 }
@@ -163,4 +166,131 @@ impl LinkableMerkleTree {
     }
 }
 
+// Anchor struct
+// TODO: Anchor should have an ERC20 attached
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Anchor {
+    chain_id: u64,
+    deposit_size: Uint256,
+    merkle_tree: MerkleTree,
+    linkable_tree: LinkableMerkleTree,
+    // used_nullifiers: Mapping<[u8; 32], bool>,
+    // poseidon: PoseidonRef,
+    // verifier: AnchorVerifierRef,
+}
 
+pub const ANCHOR: Item<Anchor> = Item::new("anchor");
+
+// Struct to save the use of "nullifiers"
+pub const NULLIFIERS: Map<Vec<u8>, bool> = Map::new("used_nullifers");
+
+// "Poseidon hasher"
+pub const POSEIDON: Item<Poseidon> = Item::new("poseidon");
+
+// "AnchorVerifier"
+pub const ANCHORVERIFIER: Item<AnchorVerifier> = Item::new("anchor_verifier");
+
+
+// MerkleTree
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct MerkleTree {
+    pub levels: u32,
+    pub current_root_index: u32,
+    pub next_index: u32,
+    // pub filled_subtrees: HashMap<u32, [u8; 32]>,
+    // pub roots: HashMap<u32, [u8; 32]>
+}
+
+// MerkleTree "filled_subtrees" Map
+pub const FILLEDSUBTREES: Map<String, [u8; 32]> = Map::new("filled_subtrees");
+
+pub fn save_subtree(store: &mut dyn Storage, k: u32, data: &[u8; 32]) -> StdResult<()> {
+    FILLEDSUBTREES.save(store, k.to_string(), data)
+}
+
+pub fn read_subtree(store: &dyn Storage, k: u32) -> StdResult<[u8; 32]> {
+    FILLEDSUBTREES.load(store, k.to_string())
+}
+
+// MerkleTree Roots Map
+pub const MERKLEROOTS: Map<String, [u8; 32]> = Map::new("merkle_roots");
+
+pub fn save_root(store: &mut dyn Storage, k: u32, data: &[u8; 32]) -> StdResult<()> {
+    MERKLEROOTS.save(store, k.to_string(), data)
+}
+
+pub fn read_root(store: &dyn Storage, k: u32) -> StdResult<[u8; 32]> {
+    MERKLEROOTS.load(store, k.to_string())
+}
+
+impl MerkleTree {
+    fn hash_left_right(
+        &self,
+        hasher: Poseidon,
+        left: [u8; 32],
+        right: [u8; 32],
+    ) -> Result<[u8; 32], ContractError> {
+        let inputs = vec![left, right];
+        hasher.hash(inputs).map_err(|_e| ContractError::HashError)
+    }
+
+    pub fn insert(
+        &mut self,
+        hasher: Poseidon,
+        leaf: [u8; 32],
+        store: &mut dyn Storage,
+    ) -> Result<u32, ContractError> {
+        let next_index = self.next_index;
+        assert!(
+            next_index != 2u32.pow(self.levels as u32),
+            "Merkle tree is full"
+        );
+
+        let mut current_index = next_index;
+        let mut current_level_hash = leaf;
+        let mut left: [u8; 32];
+        let mut right: [u8; 32];
+
+        for i in 0..self.levels {
+            if current_index % 2 == 0 {
+                left = current_level_hash;
+                right = zeroes::zeroes(i);
+                save_subtree(store, i, &current_level_hash)?;
+            } else {
+                left = read_subtree(store, i).map_err(|_| ContractError::HashError)?;
+                right = current_level_hash;
+            }
+
+            current_level_hash = self.hash_left_right(hasher.clone(), left, right)?;
+            current_index /= 2;
+        }
+
+        let new_root_index = (self.current_root_index + 1) % ROOT_HISTORY_SIZE;
+        self.current_root_index = new_root_index;
+        save_root(store, new_root_index, &current_level_hash)?;
+        self.next_index = next_index + 1;
+        Ok(next_index)
+    }
+
+    pub fn is_known_root(&self, root: [u8; 32], store: &dyn Storage) -> bool {
+        if root == [0u8; 32] {
+            return false;
+        }
+
+        let mut i = self.current_root_index;
+        for _ in 0..ROOT_HISTORY_SIZE {
+            let r = read_root(store, i).unwrap_or([0u8; 32]);
+            if r == root {
+                return true;
+            }
+
+            if i == 0 {
+                i = ROOT_HISTORY_SIZE - 1;
+            } else {
+                i -= 1;
+            }
+        }
+
+        false
+    }
+}
