1aef40ed70c548cbf46a1d02562c6c08bd68806d
guorong <dudurong009@gmail.com>
2022-03-11 07:43:56 -0700

    Update the "mixer" message & add supplementary messages

--- packages/protocol_cosmwasm/src/mixer.rs
+++ packages/protocol_cosmwasm/src/mixer.rs
@@ -1,11 +1,13 @@
 use cosmwasm_std::{Uint128, Uint256};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use cw20::Cw20ReceiveMsg;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
     pub deposit_size: Uint128,
     pub merkletree_levels: u32,
+    pub cw20_address: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -13,6 +15,7 @@ pub struct InstantiateMsg {
 pub enum ExecuteMsg {
     Deposit(DepositMsg),
     Withdraw(WithdrawMsg),
+    Receive(Cw20ReceiveMsg),
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -20,6 +23,13 @@ pub struct DepositMsg {
     pub from: Option<String>,
     pub commitment: Option<[u8; 32]>,
     pub value: Uint256,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum Cw20HookMsg {
+    /// Depcosit Cw20 tokens
+    DepositCw20 { commitment: Option<[u8; 32]> },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -31,8 +41,18 @@ pub struct WithdrawMsg {
     pub relayer: String,
     pub fee: Uint256,
     pub refund: Uint256,
+    pub cw20_address: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    GetCw20Address {},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InfoResponse {
+    pub cw20_address: String,
+}
+
