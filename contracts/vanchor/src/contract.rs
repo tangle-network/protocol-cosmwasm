bd2872df88cfedf14880f1e16fa87fe49ccf049e
guorong <dudurong009@gmail.com>
2022-03-16 02:28:53 +0800

    Add the "instantiate" entry for the "vanchor" contract

--- contracts/vanchor/src/contract.rs
+++ contracts/vanchor/src/contract.rs
@@ -5,6 +5,14 @@ use cw2::set_contract_version;
 
 use protocol_cosmwasm::vanchor::{InstantiateMsg, ExecuteMsg, QueryMsg};
 use protocol_cosmwasm::error::ContractError;
+use protocol_cosmwasm::vanchor_verifier::VAnchorVerifier;
+use protocol_cosmwasm::poseidon::Poseidon;
+use protocol_cosmwasm::zeroes::zeroes;
+
+use crate::state::{
+    save_root, save_subtree, VAnchor, LinkableMerkleTree, MerkleTree, VANCHOR, VANCHORVERIFIER,
+    NULLIFIERS, POSEIDON
+};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:cosmwasm-vanchor";
@@ -17,8 +25,53 @@ pub fn instantiate(
     info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
+    // Validation 1. Check if any funds are sent with this message.
+    if !info.funds.is_empty() {
+        return Err(ContractError::UnnecessaryFunds {  });
+    }
 
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    // Initialize the poseidon hasher
+    POSEIDON.save(deps.storage, &Poseidon::new())?;
+
+    // Initialize the VAnchor_verifier
+    VANCHORVERIFIER.save(deps.storage, &VAnchorVerifier::new())?;
+  
+    // Initialize the merkle tree
+    let merkle_tree: MerkleTree = MerkleTree {
+        levels: msg.levels,
+        current_root_index: 0,
+        next_index: 0,
+    };
+
+    // Initialize the linkable merkle tree
+    let linkable_merkle_tree = LinkableMerkleTree {
+        max_edges: msg.max_edges,
+        chain_id_list: Vec::new(),
+    };
+    // Get the "cw20_address"
+    let cw20_address = deps.api.addr_canonicalize(&msg.cw20_address)?;
+
+    // Initialize the VAnchor
+    let anchor = VAnchor {
+        owner: deps.api.addr_canonicalize(info.sender.as_str())?,
+        max_deposit_amt: msg.max_deposit_amt,
+        min_withdraw_amt: msg.min_withdraw_amt,
+        max_ext_amt: msg.max_ext_amt,
+        linkable_tree: linkable_merkle_tree,
+        merkle_tree,
+        cw20_address,
+    };
+    VANCHOR.save(deps.storage, &anchor)?;
+
+    // Initialize the "FILLED_SUBTREES" with "zero" data.
+    for i in 0..msg.levels {
+        save_subtree(deps.storage, i as u32, &zeroes(i))?;
+    }
+
+    // Initialize the (merkletree) "ROOTS" with "zero" data.
+    save_root(deps.storage, 0_u32, &zeroes(msg.levels))?;
 
     Ok(Response::new()
         .add_attribute("method", "instantiate")
@@ -48,14 +101,22 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
 mod tests {
     use super::*;
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary};
+    use cosmwasm_std::{coins, from_binary, Uint256};
 
     #[test]
     fn proper_initialization() {
+        let cw20_address = "terra1fex9f78reuwhfsnc8sun6mz8rl9zwqh03fhwf3".to_string();
         let mut deps = mock_dependencies(&[]);
 
-        let msg = InstantiateMsg { };
-        let info = mock_info("creator", &coins(1000, "earth"));
+        let msg = InstantiateMsg {
+            max_edges: 0,
+            levels: 0,
+            max_deposit_amt: Uint256::zero(),
+            min_withdraw_amt: Uint256::zero(),
+            max_ext_amt: Uint256::zero(),
+            cw20_address: cw20_address,
+        };
+        let info = mock_info("creator", &[]);
 
         // we can just call .unwrap() to assert this was a success
         let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
--- packages/protocol_cosmwasm/src/vanchor.rs
+++ packages/protocol_cosmwasm/src/vanchor.rs
@@ -1,9 +1,15 @@
+use cosmwasm_std::{Uint256};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    // TODO
+    pub levels: u32,
+    pub max_edges: u32,
+    pub cw20_address: String,
+    pub max_deposit_amt: Uint256,
+    pub min_withdraw_amt: Uint256,
+    pub max_ext_amt: Uint256,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
