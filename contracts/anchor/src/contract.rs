bc19ff39ffa5b51040577cfbbb31764584f3af63
guorong <dudurong009@gmail.com>
2022-03-19 01:36:47 +0800

    Add 2 more tests for "invalid merkle roots" & "invalid proof" cases

--- contracts/anchor/src/contract.rs
+++ contracts/anchor/src/contract.rs
@@ -408,7 +408,7 @@ mod tests {
     }
 
     #[test]
-    fn test_anchor_deposit_cw20() {
+    fn test_anchor_should_be_able_to_deposit() {
         let cw20_address = "terra1fex9f78reuwhfsnc8sun6mz8rl9zwqh03fhwf3".to_string();
 
         let mut deps = mock_dependencies(&coins(2, "token"));
@@ -426,7 +426,7 @@ mod tests {
 
         let _ = instantiate(deps.as_mut(), env, info, instantiate_msg).unwrap();
 
-        // Initialize the mixer
+        // Initialize the anchor.
         let params = get_poseidon_bn254_x5_5();
         let left_input = Fr::one().into_repr().to_bytes_le();
         let right_input = Fr::one().double().into_repr().to_bytes_le();
@@ -456,7 +456,186 @@ mod tests {
     }
 
     #[test]
-    fn test_anchor_withdraw_cw20_wasm_utils() {
+    fn test_anchor_fail_when_any_byte_is_changed_in_proof() {
+        let curve = Curve::Bn254;
+        let (pk_bytes, _) = crate::test_util::setup_environment(curve);
+        let src_chain_id = compute_chain_id_type(1u64, &COSMOS_CHAIN_TYPE);
+        let recipient_bytes = [2u8; 32];
+        let relayer_bytes = [0u8; 32];
+        let fee_value = 0;
+        let refund_value = 0;
+        let commitment_bytes = [0u8; 32];
+        let commitment_element = Element::from_bytes(&commitment_bytes);
+
+        // Setup zk circuit for withdraw
+        let (proof_bytes, root_elements, nullifier_hash_element, leaf_element) =
+            crate::test_util::setup_wasm_utils_zk_circuit(
+                curve,
+                truncate_and_pad(&recipient_bytes),
+                truncate_and_pad(&relayer_bytes),
+                commitment_bytes,
+                pk_bytes.clone(),
+                src_chain_id as u128,
+                fee_value,
+                refund_value,
+            );
+
+        let cw20_address = "terra1fex9f78reuwhfsnc8sun6mz8rl9zwqh03fhwf3".to_string();
+        let mut deps = mock_dependencies(&coins(2, "token"));
+
+        // Initialize the contract
+        let env = mock_env();
+        let info = mock_info("anyone", &[]);
+        let instantiate_msg = InstantiateMsg {
+            max_edges: 2,
+            chain_id: 1,
+            levels: 30,
+            deposit_size: Uint128::from(1_000_000_u128),
+            cw20_address: cw20_address.clone(),
+        };
+
+        let _ = instantiate(deps.as_mut(), env, info, instantiate_msg).unwrap();
+
+        // Should "deposit" cw20 tokens with success.
+        let info = mock_info(cw20_address.as_str(), &[]);
+        let deposit_cw20_msg = ExecuteMsg::Receive(Cw20ReceiveMsg {
+            sender: cw20_address.clone(),
+            amount: Uint128::from(1_000_000_u128),
+            msg: to_binary(&Cw20HookMsg::DepositCw20 {
+                commitment: Some(leaf_element.0),
+            })
+            .unwrap(),
+        });
+
+        let response = execute(deps.as_mut(), mock_env(), info, deposit_cw20_msg).unwrap();
+        assert_eq!(
+            response.attributes,
+            vec![attr("method", "deposit_cw20"), attr("result", "0")]
+        );
+
+        let on_chain_root = crate::state::read_root(&deps.storage, 1).unwrap();
+        let local_root = root_elements[0].0;
+        assert_eq!(on_chain_root, local_root);
+    
+        // Invalid withdraw proof leads to failure result.
+        let mut wrong_proof_bytes = proof_bytes.clone();
+        wrong_proof_bytes[0] = 0;
+
+        let mut roots = vec![];
+        for i in 0..root_elements.len() {
+            roots.push(root_elements[i].0);
+        }
+
+        let withdraw_msg = WithdrawMsg {
+            proof_bytes: wrong_proof_bytes,
+            roots: roots.clone(),
+            nullifier_hash: nullifier_hash_element.0,
+            recipient: hex::encode(recipient_bytes.to_vec()),
+            relayer: hex::encode(relayer_bytes.to_vec()),
+            fee: cosmwasm_std::Uint256::from(fee_value),
+            refund: cosmwasm_std::Uint256::from(refund_value),
+            commitment: commitment_element.0,
+            cw20_address: cw20_address.clone(),
+        };
+        let info = mock_info("withdraw", &[]);
+        assert!(
+            withdraw(deps.as_mut(), info, withdraw_msg).is_err(),
+            "Should fail with wrong proof bytes"
+        );
+    }
+
+
+    #[test]
+    fn test_anchor_fail_when_invalid_merkle_roots() {
+
+        let curve = Curve::Bn254;
+        let (pk_bytes, _) = crate::test_util::setup_environment(curve);
+        let src_chain_id = compute_chain_id_type(1u64, &COSMOS_CHAIN_TYPE);
+        let recipient_bytes = [2u8; 32];
+        let relayer_bytes = [0u8; 32];
+        let fee_value = 0;
+        let refund_value = 0;
+        let commitment_bytes = [0u8; 32];
+        let commitment_element = Element::from_bytes(&commitment_bytes);
+
+        // Setup zk circuit for withdraw
+        let (proof_bytes, root_elements, nullifier_hash_element, leaf_element) =
+            crate::test_util::setup_wasm_utils_zk_circuit(
+                curve,
+                truncate_and_pad(&recipient_bytes),
+                truncate_and_pad(&relayer_bytes),
+                commitment_bytes,
+                pk_bytes.clone(),
+                src_chain_id as u128,
+                fee_value,
+                refund_value,
+            );
+
+        let cw20_address = "terra1fex9f78reuwhfsnc8sun6mz8rl9zwqh03fhwf3".to_string();
+        let mut deps = mock_dependencies(&coins(2, "token"));
+
+        // Initialize the contract
+        let env = mock_env();
+        let info = mock_info("anyone", &[]);
+        let instantiate_msg = InstantiateMsg {
+            max_edges: 2,
+            chain_id: 1,
+            levels: 30,
+            deposit_size: Uint128::from(1_000_000_u128),
+            cw20_address: cw20_address.clone(),
+        };
+
+        let _ = instantiate(deps.as_mut(), env, info, instantiate_msg).unwrap();
+
+        // Should "deposit" cw20 tokens with success.
+        let info = mock_info(cw20_address.as_str(), &[]);
+        let deposit_cw20_msg = ExecuteMsg::Receive(Cw20ReceiveMsg {
+            sender: cw20_address.clone(),
+            amount: Uint128::from(1_000_000_u128),
+            msg: to_binary(&Cw20HookMsg::DepositCw20 {
+                commitment: Some(leaf_element.0),
+            })
+            .unwrap(),
+        });
+
+        let response = execute(deps.as_mut(), mock_env(), info, deposit_cw20_msg).unwrap();
+        assert_eq!(
+            response.attributes,
+            vec![attr("method", "deposit_cw20"), attr("result", "0")]
+        );
+
+        let on_chain_root = crate::state::read_root(&deps.storage, 1).unwrap();
+        let local_root = root_elements[0].0;
+        assert_eq!(on_chain_root, local_root);
+    
+        // Invalid root_element leads to failure.
+        let mut wrong_roots = vec![];
+        for i in 0..root_elements.len() {
+            wrong_roots.push(root_elements[i].0);
+        }
+        wrong_roots[0][0] = 0;
+
+        let withdraw_msg = WithdrawMsg {
+            proof_bytes: proof_bytes.clone(),
+            roots: wrong_roots,
+            nullifier_hash: nullifier_hash_element.0,
+            recipient: hex::encode(recipient_bytes.to_vec()),
+            relayer: hex::encode(relayer_bytes.to_vec()),
+            fee: cosmwasm_std::Uint256::from(fee_value),
+            refund: cosmwasm_std::Uint256::from(refund_value),
+            commitment: commitment_element.0,
+            cw20_address: cw20_address.clone(),
+        };
+        let info = mock_info("withdraw", &[]);
+        let err = withdraw(deps.as_mut(), info, withdraw_msg).unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            "Generic error: Root is not known".to_string()
+        );
+    }
+
+    #[test]
+    fn test_anchor_works_with_wasm_utils() {
         let curve = Curve::Bn254;
         let (pk_bytes, _) = crate::test_util::setup_environment(curve);
         let src_chain_id = compute_chain_id_type(1u64, &COSMOS_CHAIN_TYPE);
@@ -517,31 +696,10 @@ mod tests {
         let local_root = root_elements[0].0;
         assert_eq!(on_chain_root, local_root);
 
-        // Invalid withdraw proof leads to failure result.
-        let mut wrong_proof_bytes = proof_bytes.clone();
-        wrong_proof_bytes[0] = 0;
-
         let mut roots = vec![];
         for i in 0..root_elements.len() {
             roots.push(root_elements[i].0);
         }
-
-        let withdraw_msg = WithdrawMsg {
-            proof_bytes: wrong_proof_bytes,
-            roots: roots.clone(),
-            nullifier_hash: nullifier_hash_element.0,
-            recipient: hex::encode(recipient_bytes.to_vec()),
-            relayer: hex::encode(relayer_bytes.to_vec()),
-            fee: cosmwasm_std::Uint256::from(fee_value),
-            refund: cosmwasm_std::Uint256::from(refund_value),
-            commitment: commitment_element.0,
-            cw20_address: cw20_address.clone(),
-        };
-        let info = mock_info("withdraw", &[]);
-        assert!(
-            withdraw(deps.as_mut(), info, withdraw_msg).is_err(),
-            "Should fail with wrong proof bytes"
-        );
 
         // Should "withdraw" cw20 tokens with success.
         let withdraw_msg = WithdrawMsg {
@@ -585,7 +743,7 @@ mod tests {
     }
 
     #[test]
-    fn test_anchor_withdraw_cw20_native() {
+    fn test_anchor_works() {
         let curve = Curve::Bn254;
         let (pk_bytes, _) = crate::test_util::setup_environment(curve);
         let recipient_bytes = [1u8; 32];
@@ -594,7 +752,7 @@ mod tests {
         let refund_value = 0;
         let src_chain_id = compute_chain_id_type(1, &COSMOS_CHAIN_TYPE);
         let commitment_bytes = vec![0u8; 32];
-        let commit_element = Element::from_bytes(&commitment_bytes);
+        let commitment_element = Element::from_bytes(&commitment_bytes);
 
         // Setup zk circuit for withdraw
         let (proof_bytes, root_elements, nullifier_hash_element, leaf_element) =
@@ -646,31 +804,6 @@ mod tests {
         let local_root = root_elements[0].0;
         assert_eq!(on_chain_root, local_root);
 
-        // Invalid root_element leads to failure.
-        let mut wrong_roots = vec![];
-        for i in 0..root_elements.len() {
-            wrong_roots.push(root_elements[i].0);
-        }
-        wrong_roots[0][0] = 0;
-
-        let withdraw_msg = WithdrawMsg {
-            proof_bytes: proof_bytes.clone(),
-            roots: wrong_roots,
-            nullifier_hash: nullifier_hash_element.0,
-            recipient: hex::encode(recipient_bytes.to_vec()),
-            relayer: hex::encode(relayer_bytes.to_vec()),
-            fee: cosmwasm_std::Uint256::from(fee_value),
-            refund: cosmwasm_std::Uint256::from(refund_value),
-            commitment: commit_element.0,
-            cw20_address: cw20_address.clone(),
-        };
-        let info = mock_info("withdraw", &[]);
-        let err = withdraw(deps.as_mut(), info, withdraw_msg).unwrap_err();
-        assert_eq!(
-            err.to_string(),
-            "Generic error: Root is not known".to_string()
-        );
-
         // Should "withdraw" cw20 tokens with success.
         let mut roots = vec![];
         for elem in root_elements {
@@ -684,7 +817,7 @@ mod tests {
             relayer: hex::encode(relayer_bytes.to_vec()),
             fee: cosmwasm_std::Uint256::from(fee_value),
             refund: cosmwasm_std::Uint256::from(refund_value),
-            commitment: commit_element.0,
+            commitment: commitment_element.0,
             cw20_address: cw20_address.clone(),
         };
         let info = mock_info("withdraw", &[]);
