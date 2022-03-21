5fd077fc75bdf8f0c8d36e272d36916a68980c69
guorong <dudurong009@gmail.com>
2022-03-21 22:44:28 +0800

    Add the unit test for "too small out amounts" case in "vanchor" contract

--- contracts/vanchor/src/tests.rs
+++ contracts/vanchor/src/tests.rs
@@ -833,3 +833,202 @@ fn test_vanchor_should_not_complete_withdraw_if_out_amount_sum_is_too_big() {
         "Generic error: Invalid transaction proof".to_string()
     );
 }
+
+#[test]
+fn test_vanchor_should_not_complete_withdraw_if_out_amount_sum_is_too_small() {
+    // Instantiate the "vanchor" contract.
+    let mut deps = mock_dependencies(&[]);
+
+    let msg = InstantiateMsg {
+        chain_id: CHAIN_ID,
+        max_edges: MAX_EDGES,
+        levels: LEVELS,
+        max_deposit_amt: Uint256::from(MAX_DEPOSIT_AMT),
+        min_withdraw_amt: Uint256::from(MIN_WITHDRAW_AMT),
+        max_ext_amt: Uint256::from(MAX_EXT_AMT),
+        max_fee: Uint256::from(MAX_FEE),
+        cw20_address: CW20_ADDRESS.to_string(),
+    };
+    let info = mock_info("creator", &[]);
+
+    // we can just call .unwrap() to assert this was a success
+    let _ = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+    // Initialize the vanchor
+    let (pk_bytes, _) = crate::test_util::setup_environment(Curve::Bn254);
+    let transactor_bytes = [1u8; 32];
+    let recipient_bytes = [2u8; 32];
+    let relayer_bytes = [0u8; 32];
+    let ext_amount = 10_i128;
+    let fee = 0_u128;
+
+    let public_amount = 10_i128;
+
+    let chain_type = [4, 0];
+    let chain_id = compute_chain_id_type(1, &chain_type);
+    let in_chain_ids = [chain_id; 2];
+    let in_amounts = [0, 0];
+    let in_indices = [0, 1];
+    let out_chain_ids = [chain_id; 2];
+    let out_amounts = [10, 0];
+
+    let in_utxos = crate::test_util::setup_utxos(in_chain_ids, in_amounts, Some(in_indices));
+    // We are adding indices to out utxos, since they will be used as an input utxos in next transaction
+    let out_utxos = crate::test_util::setup_utxos(out_chain_ids, out_amounts, Some(in_indices));
+
+    let output1 = out_utxos[0].commitment.into_repr().to_bytes_le();
+    let output2 = out_utxos[1].commitment.into_repr().to_bytes_le();
+
+    let ext_data = ExtData {
+        recipient: hex::encode(recipient_bytes),
+        relayer: hex::encode(relayer_bytes),
+        ext_amount: ext_amount.to_string(),
+        fee: fee.to_string(),
+        encrypted_output1: element_encoder(&output1),
+        encrypted_output2: element_encoder(&output2),
+    };
+
+    let mut ext_data_args = Vec::new();
+    let recipient_bytes = element_encoder(&hex::decode(&ext_data.recipient).unwrap());
+    let relayer_bytes = element_encoder(&hex::decode(&ext_data.relayer).unwrap());
+    let fee_bytes = element_encoder(&fee.to_le_bytes());
+    let ext_amt_bytes = element_encoder(&ext_amount.to_le_bytes());
+    ext_data_args.extend_from_slice(&recipient_bytes);
+    ext_data_args.extend_from_slice(&relayer_bytes);
+    ext_data_args.extend_from_slice(&ext_amt_bytes);
+    ext_data_args.extend_from_slice(&fee_bytes);
+    ext_data_args.extend_from_slice(&ext_data.encrypted_output1);
+    ext_data_args.extend_from_slice(&ext_data.encrypted_output2);
+
+    let ext_data_hash = keccak_256(&ext_data_args);
+    let custom_roots = Some([[0u8; 32]; 2].map(|x| x.to_vec()));
+    let (proof, public_inputs) = crate::test_util::setup_zk_circuit(
+        public_amount,
+        chain_id,
+        ext_data_hash.to_vec(),
+        in_utxos,
+        out_utxos.clone(),
+        custom_roots,
+        pk_bytes,
+    );
+
+    // Deconstructing public inputs
+    let (_chain_id, public_amount, root_set, nullifiers, commitments, ext_data_hash) =
+        crate::test_util::deconstruct_public_inputs_el(&public_inputs);
+
+    // Constructing proof data
+    let root_set = root_set.into_iter().map(|v| v.0).collect();
+    let nullifiers = nullifiers.into_iter().map(|v| v.0).collect();
+    let commitments = commitments.into_iter().map(|v| v.0).collect();
+    let proof_data = ProofData::new(
+        proof,
+        public_amount.0,
+        root_set,
+        nullifiers,
+        commitments,
+        ext_data_hash.0,
+    );
+
+    // Should "transact" with success.
+    let info = mock_info(CW20_ADDRESS, &[]);
+    let deposit_cw20_msg = ExecuteMsg::Receive(Cw20ReceiveMsg {
+        sender: hex::encode(transactor_bytes),
+        amount: Uint128::from(10_u128),
+        msg: to_binary(&Cw20HookMsg::Transact {
+            proof_data: proof_data,
+            ext_data: ext_data,
+        })
+        .unwrap(),
+    });
+
+    // Deposit "10" cw20 tokens.
+    let _ = execute(deps.as_mut(), mock_env(), info, deposit_cw20_msg).unwrap();
+
+    // Prepare the "withdraw" data.
+    let (pk_bytes, _) = crate::test_util::setup_environment(Curve::Bn254);
+
+    let ext_amount = -5_i128;
+    let fee = 2_u128;
+
+    let public_amount = -7_i128;
+
+    let chain_id = compute_chain_id_type(CHAIN_ID, &chain_type);
+    let out_chain_ids = [CHAIN_ID; 2];
+    // Too small out amounts
+    let out_amounts = [1, 0];
+
+    // "in_utxos" become the "out_utxos" of last transact.
+    let in_utxos = out_utxos;
+    let out_utxos = crate::test_util::setup_utxos(out_chain_ids, out_amounts, None);
+
+    let output1 = out_utxos[0].commitment.into_repr().to_bytes_le();
+    let output2 = out_utxos[1].commitment.into_repr().to_bytes_le();
+    let ext_data = ExtData {
+        recipient: hex::encode(recipient_bytes),
+        relayer: hex::encode(relayer_bytes),
+        ext_amount: ext_amount.to_string(),
+        fee: fee.to_string(),
+        encrypted_output1: element_encoder(&output1),
+        encrypted_output2: element_encoder(&output2),
+    };
+
+    let mut ext_data_args = Vec::new();
+    let recipient_bytes = element_encoder(&hex::decode(&ext_data.recipient).unwrap());
+    let relayer_bytes = element_encoder(&hex::decode(&ext_data.relayer).unwrap());
+    let fee_bytes = element_encoder(&fee.to_le_bytes());
+    let ext_amt_bytes = element_encoder(&ext_amount.to_le_bytes());
+    ext_data_args.extend_from_slice(&recipient_bytes);
+    ext_data_args.extend_from_slice(&relayer_bytes);
+    ext_data_args.extend_from_slice(&ext_amt_bytes);
+    ext_data_args.extend_from_slice(&fee_bytes);
+    ext_data_args.extend_from_slice(&ext_data.encrypted_output1);
+    ext_data_args.extend_from_slice(&ext_data.encrypted_output2);
+
+    let ext_data_hash = keccak_256(&ext_data_args);
+
+    let (proof, public_inputs) = crate::test_util::setup_zk_circuit(
+        public_amount,
+        chain_id,
+        ext_data_hash.to_vec(),
+        in_utxos,
+        out_utxos,
+        None,
+        pk_bytes,
+    );
+
+    // Deconstructing public inputs
+    let (_chain_id, public_amount, root_set, nullifiers, commitments, ext_data_hash) =
+        crate::test_util::deconstruct_public_inputs_el(&public_inputs);
+
+    // Constructing proof data
+    let root_set = root_set.into_iter().map(|v| v.0).collect();
+    let nullifiers = nullifiers.into_iter().map(|v| v.0).collect();
+    let commitments = commitments.into_iter().map(|v| v.0).collect();
+
+    let proof_data = ProofData::new(
+        proof,
+        public_amount.0,
+        root_set,
+        nullifiers,
+        commitments,
+        ext_data_hash.0,
+    );
+
+    // Should fail with "Invalid ext amount".
+    let info = mock_info(CW20_ADDRESS, &[]);
+    let withdraw_cw20_msg = ExecuteMsg::Receive(Cw20ReceiveMsg {
+        sender: hex::encode(transactor_bytes),
+        amount: Uint128::zero(),
+        msg: to_binary(&Cw20HookMsg::Transact {
+            proof_data: proof_data,
+            ext_data: ext_data,
+        })
+        .unwrap(),
+    });
+
+    let err = execute(deps.as_mut(), mock_env(), info, withdraw_cw20_msg).unwrap_err();
+    assert_eq!(
+        err.to_string(),
+        "Generic error: Invalid transaction proof".to_string()
+    );
+}
