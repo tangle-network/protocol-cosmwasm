947067b24a226d94e28c9c5daf2a3f7289361575
guorong <dudurong009@gmail.com>
2022-03-26 00:21:50 +0800

    Add the validation function "is_valid_address" in "tokenwrapper"

--- contracts/tokenwrapper/src/contract.rs
+++ contracts/tokenwrapper/src/contract.rs
@@ -1,7 +1,7 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128,
+    to_binary, Addr, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128,
 };
 use cw2::set_contract_version;
 
@@ -12,6 +12,7 @@ use cw20_base::allowances::{
 use cw20_base::contract::{
     execute_burn, execute_mint, execute_send, execute_transfer, query_balance, query_token_info,
 };
+use cw20_base::msg::{QueryMsg as Cw20QueryMsg};
 use cw20_base::state::{MinterData, TokenInfo, TOKEN_INFO};
 
 use protocol_cosmwasm::error::ContractError;
@@ -105,6 +106,16 @@ pub fn execute(
         } => Ok(execute_send_from(
             deps, env, info, owner, contract, amount, msg,
         )?),
+    }
+}
+
+fn is_valid_address(deps: DepsMut, token_address: Addr) -> StdResult<bool> {
+    let token_info_query: StdResult<TokenInfo> = deps
+        .querier
+        .query_wasm_smart(token_address, &Cw20QueryMsg::TokenInfo {});
+    match token_info_query {
+        Ok(_v) => Ok(true),
+        Err(e) => Err(e),
     }
 }
 
